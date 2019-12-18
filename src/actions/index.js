829fda154cfde9e362bc21042a3600c1da6d7d85
SleeOne <krevetko253@gmail.com>
2019-12-18 09:55:47 +0300

    Little improvement

--- src/actions/index.js
+++ src/actions/index.js
@@ -14,7 +14,7 @@ export const getImages = () => async (dispatch) => {
     }
 
     try {
-        const accessKey = '896d4f52c589547b2134bd75ed48742db637fa51810b49b607e37e46ab2c0043';
+        const accessKey = 'bb6aaa964afd79399998473acf12e23e44d103f4e132586d9776371d3abbd905';
 
         const unsplash = new Unsplash({accessKey: accessKey});
         unsplash.photos.listPhotos(2, 30, 'latest')
